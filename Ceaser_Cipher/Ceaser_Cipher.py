ecd27775007504494d38d5efa7da1e9aeecb7bfa
Dustine Hacbang <dustinehacbang526@gmail.com>
2025-12-25 08:39:04 -0800

    Fix Ceaser Cipher implementation by adding print statement for shifted alphabet

--- Ceaser_Cipher/Ceaser_Cipher.py
+++ Ceaser_Cipher/Ceaser_Cipher.py
@@ -1,11 +1,41 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = ''
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# shift = 5
+# shifted_alphabet = ''
 
-for i in range(len(alphabet)):
-    new_index = i + shift
-    if new_index >= len(alphabet):
-        new_index = new_index - len(alphabet)
-    shifted_alphabet += alphabet[new_index]
+# for i in range(len(alphabet)):
+#     new_index = i + shift
+#     if new_index >= len(alphabet):
+#         new_index = new_index - len(alphabet)
+#     shifted_alphabet += alphabet[new_index]
+#     # print(shifted_alphabet)
 
-print(shifted_alphabet)
+# # print(shifted_alphabet)
+
+# translation_table = str.maketrans(alphabet, shifted_alphabet)
+
+def caesar(text, shift, encrypt=True):
+
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+encrypted_text = encrypt('freeCodeCamp', 3)
+print(encrypted_text)
